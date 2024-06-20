9d3e51c5bba583006c9133ae6cd5de1f6b8950e9
yjung <yjung@kentech.ac.kr>
2024-06-20 15:18:09 +0900

    Contour_Crate_Rout

--- Contour_Crate_Rout.m
+++ Contour_Crate_Rout.m
@@ -5,7 +5,6 @@ clear;clc;close all
 data_dir = 'C:\Users\user\Desktop\MATLAB\Tubular_battery';
 data_file = fullfile(data_dir, 'Cylinder_Sweep_Crate_Rout_Result.mat');
 load(data_file);
-% load Cylinder_Sweep_Crate_Rout_Result.mat
 
 C_rate_vec = data.C_rate;
 D_out_vec = 2 * data.R_out;
