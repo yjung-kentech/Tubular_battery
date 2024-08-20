5064473225f8567d66e38007cdc016bba5d33057
yjung <yjung@kentech.ac.kr>
2024-08-20 11:40:54 +0900

    Figure1.m
    Tmax, Tavg

--- Figure1.m
+++ Figure1.m
@@ -50,25 +50,27 @@ ModelUtil.remove('model2'); % 모델 2 닫기
 
 % Temperature plot
 figure;
-red = [0.8500, 0.3250, 0.0980];
-blue = [0, 0.4470, 0.7410];
+lw = 1; % Desired line width
+color1 = [0.8500, 0.3250, 0.0980]; % Orange
+color2 = [0, 0.4470, 0.7410]; % Blue
+interval = 10;
 
-plot(time1, T_max1, 'red', 'LineWidth', 1, 'DisplayName', 'Tube T_{max}');
+plot(time1, T_max1, 'Color', color1, 'LineWidth', lw, 'LineStyle', '-', 'Marker', 'o', 'MarkerIndices', 1:interval:length(T_max1), 'DisplayName', 'Tube T_{max}');
 hold on
-plot(time1, T_avg1, 'red--', 'LineWidth', 1, 'DisplayName', 'Tube T_{avg}');
-hold on
-plot(time2, T_max2, 'blue', 'LineWidth', 1, 'DisplayName', 'Cylinder T_{max}');
-hold on
-plot(time2, T_avg2, 'blue--', 'LineWidth', 1, 'DisplayName', 'Cylinder T_{avg}');
+plot(time1, T_avg1, 'Color', color2, 'LineWidth', lw, 'LineStyle', '-', 'Marker', 'o', 'MarkerIndices', 1:interval:length(T_avg1), 'DisplayName', 'Tube T_{avg}');
+plot(time2, T_max2, 'Color', color1, 'LineWidth', lw, 'LineStyle', '-', 'Marker', 's', 'MarkerIndices', 1:interval:length(T_max2), 'DisplayName', 'Cylinder T_{max}');
+plot(time2, T_avg2, 'Color', color2, 'LineWidth', lw, 'LineStyle', '-', 'Marker', 's', 'MarkerIndices', 1:interval:length(T_avg2), 'DisplayName', 'Cylinder T_{avg}');
 hold off
 
 % yline(50, 'k--', 'LineWidth', 1, 'DisplayName', '50 ^oC');
 
 title('4680 6C');
-xlabel('Time (min)');
-ylabel('Temperature (degC)');
+xlabel('Time [min]');
+ylabel('Temperature [degC]');
 legend('show');
 grid on;
 
 xlim([0 20]);
 xticks(0:5:20);
+
+exportgraphics(gcf, 'figure1.png', 'Resolution', 300);
